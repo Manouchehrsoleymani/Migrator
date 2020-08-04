3bfad93a85f452cdda124cbdeb4eb1ab3153ce4b
root <root@localhost.localdomain>
2020-08-05 00:02:58 +0430

    Attributes Model is created

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class attributes(models.Model):
+    path = models.CharField(max_length=255)
+    file_name = models.CharField(max_length=128)
+    size = models.BigIntegerField()
+    file_type = models.CharField(max_length=20)
+    create_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    access_time = models.DateTimeField()
